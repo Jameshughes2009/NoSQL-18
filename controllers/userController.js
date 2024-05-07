8cf3cd367ccc833305d01e757ec8e08ca54cf02e
James Hughes <jameshughes2009@hotmail.com>
2024-05-07 16:04:43 -0400

    working on userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,5 +3,16 @@ const {User} = require('../models');
 const userController = {
     getAllUsers(req, res){
         User.find({})
+            .then(userData => res.json(userData))
+            .catch(err => res.status(500).json(err));
+    },
+
+
+    getUserByID(req, res) {
+        User.findById(req.params.userId)
+            .then(userData => res.json(userData))
+            .catch(err => res.status(500)).json(err)
     }
-}
+};
+
+module.exports = userController;
