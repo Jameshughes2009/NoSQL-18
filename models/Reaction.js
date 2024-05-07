e7c4e17607f18f419085841e213cf28db6d88f61
James Hughes <jameshughes2009@hotmail.com>
2024-05-07 14:54:26 -0400

    reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,11 +1,29 @@
 const {Schema,Types} = require('mongoose');
-const { type } = require('os');
 
 const reactionSchema = new Schema(
     {
         reactionId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
         }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
     }
-)
+);
