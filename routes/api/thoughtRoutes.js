af5142f3ad7f688be86a6e4dab23910bda47ac11
James Hughes <jameshughes2009@hotmail.com>
2024-05-09 14:31:18 -0400

    delete function

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,7 +9,7 @@ const {
 
 router.route('/').get(getAllThoughts).post(createThought);
 
-router.route
+router.route('/:thoughtId').get(getOneThought).put(updateThought).delete(deleteThought);
 
 
 module.exports = router;
